a14abc0468e946558154907082f28a5661f3860f
Innocent-Bern <innocentbern22@gmail.com>
2024-07-04 13:19:41 +0300

    task-3

--- controllers/UsersController.js
+++ controllers/UsersController.js
@@ -2,7 +2,7 @@ import dbClient from '../utils/db';
 import sha1 from 'sha1';
 
 class UsersController {
-    static async postNew(req, res) {
+    static postNew(req, res) {
         const { email } = req.body;
         const { password } = req.body;
 
@@ -15,13 +15,13 @@ class UsersController {
             return;
         }
         const users = dbClient.db.collection('users');
-        await users.findOne({ email }, async (err, user) => {
+        users.findOne({ email }, (err, user) => {
             if (err) throw err;
             if (user) {
                 res.status(400).json({ error: 'Already exists' });
             } else {
                 try {
-                    await users.insertOne({
+                    users.insertOne({
                         email: email,
                         password: sha1(password)
                     }).then((response) => {
