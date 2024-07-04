5efb10171a243f7044ac80cee13571ec1cb7a599
Innocent-Bern <innocentbern22@gmail.com>
2024-07-04 13:09:42 +0300

    fixed typo

--- routes/index.js
+++ routes/index.js
@@ -6,6 +6,6 @@ const router = Router();
 
 router.get('/status', AppController.getStatus);
 router.get('/stats', AppController.getStats);
-router.post('/user', UsersController.postNew);
+router.post('/users', UsersController.postNew);
 
 module.exports = router;
